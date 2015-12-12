e0aaa68779794c45c8b4c2399cc114da6b6c0a19
Andrew <andrewklaus@gmail.com>
2015-12-11 21:33:02 -0700

    Fixed heading

--- arch-laptop/generate.sh
+++ arch-laptop/generate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-echo '### Generated with "pacman -Qe | awk ''{print $1}'' > package_list.txt' > package_list.txt 
+echo '### Generated with "pacman -Qe | awk ''{print $1}'' > package_list.txt"' > package_list.txt 
 pacman -Qe | awk '{print $1}' >> package_list.txt
